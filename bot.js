e16965c86f791e28458748b2015d4b4d59e60000
Trivernis <trivernis@hotmail.de>
2018-12-23 02:25:31 +0100

    Added supporting classes
    - added class GuildHandler -> use this for commands that sould be run on a server
    - added class Servant -> handles commands, used by GuildHandler
    - added connected getter to DJ
    - changed from checking for Connection to checking if connected
    - added possibility to assign a VoiceChannel at connecting
    - commands now contain the prefix instead of being a child element of it
    - added passing additional arguments as third parameter of the command callback
    - removed complex command parsing function, rewrote it with better pattern matching
    - removed music commands from bot.js and moved them to GuildHandler class
    - changed from cmd.createCommand to createGlobalCommand

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js"),
     logger = require('./lib/logging').getLogger(),
     music = require('./lib/music'),
     cmd = require("./lib/cmd"),
+    guilding = require('./lib/guilding'),
     client = new Discord.Client(),
     args = require('args-parser')(process.argv),
     authToken = args.token,
@@ -32,108 +33,15 @@ function savePlaylist(url, name) {
 }
 
 function registerCommands() {
-    cmd.createCommand(prefix, 'play', (msg, argv) => {
-        let vc = msg.member.voiceChannel;
-        let url = argv['url'];
-        if (!url) return 'No url given.';
-        if (!url.match(/http/g)) {
-            if (savedplaylists[url]) {
-                url = savedplaylists[url];
-            }
-        }
-        try {
-            return music.play(vc, url);
-        } catch(err) {
-            logger.error(err);
-            msg.reply(`${JSON.stringify(err)}`);
-        }
-    }, ['url'], "Adds the url to the YouTube video/playlist into the queue.");
-
-    cmd.createCommand(prefix, 'playnext', (msg, argv) => {
-        let vc = msg.member.voiceChannel;
-        let url = argv['url'];
-        if (!url) return 'No url given.';
-        if (!url.match(/http/g)) {
-            if (savedplaylists[url]) {
-                url = savedplaylists[url];
-            }
-        }
-        try {
-            return music.playnext(vc, url);
-        } catch(err) {
-            logger.error(err);
-            msg.reply(`${JSON.stringify(err)}`);
-        }
-    }, ['url'], "Plays the YouTube video after the currently playing song.");
-
-    cmd.createCommand(prefix, 'ping', () => {
+    cmd.createGlobalCommand(prefix + 'ping', () => {
        return 'Pong!';
     }, [], "Try it yourself.");
 
-    cmd.createCommand(prefix, 'join', (msg) => {
-        if (msg.member.voiceChannel) {
-            music.connect(msg.member.voiceChannel);
-        }
-        else {
-            msg.reply("You are not connected to a voicechannel.");
-        }
-    }, [], "Joins the VC you are in.");
+    cmd.createGlobalCommand(prefix + 'repeatafterme', (msg, argv, args) => {
+        return args.join(' ');
+    },[], "Repeats what you say");
 
-    cmd.createCommand(prefix, 'stop', (msg) => {
-        let gid = msg.guild.id;
-        music.stop(gid);
-    }, [], "Stops playling music and leavs.");
-
-    cmd.createCommand(prefix, 'pause', (msg) => {
-        let gid = msg.guild.id;
-        music.pause(gid);
-    }, [], "Pauses playing.");
-
-    cmd.createCommand(prefix, 'resume', (msg) => {
-        let gid = msg.guild.id;
-        music.resume(gid);
-    }, [], "Resumes playing.");
-
-    cmd.createCommand(prefix, 'skip', (msg) => {
-        let gid = msg.guild.id;
-        music.skip(gid);
-    }, [], "Skips the current song.");
-
-    cmd.createCommand(prefix, 'clear', (msg) => {
-        let gid = msg.guild.id;
-        music.clearQueue(gid);
-        return "All songs have been deleted, commander :no_mouth:  "
-    }, [],"Clears the playlist.");
-
-    cmd.createCommand(prefix, 'playlist', (msg) => {
-        let gid = msg.guild.id;
-        let songs = music.getQueue(gid);
-        logger.debug(`found ${songs.length} songs`);
-        let songlist = `**${songs.length} Songs in playlist**\n`;
-        for (let i = 0; i < songs.length; i++) {
-            if (i > 10) break;
-            songlist += songs[i] + '\n';
-        }
-        return songlist;
-    }, [], "Shows the next ten songs.");
-
-    cmd.createCommand(prefix, 'shuffle', (msg) => {
-        let gid = msg.guild.id;
-        music.shuffle(gid);
-        return "The queue has successfully been shuffled :slight_smile:"
-    }, [], "Shuffles the playlist.");
-
-    cmd.createCommand(prefix, 'current', (msg) => {
-        let gid = msg.guild.id;
-        let song = music.nowPlaying(gid);
-        return `Playing: ${song.title}\n ${song.url}`;
-    }, [], "Shows the currently playing song.");
-
-    cmd.createCommand(prefix, 'repeatafterme', (msg, argv) => {
-        return argv['word'];
-    }, ['word'], "Repeats a single word you say.");
-
-    cmd.createCommand(prefix, 'save', (msg, argv) => {
+    cmd.createGlobalCommand(prefix + 'save', (msg, argv) => {
         savePlaylist(argv['url'], argv['name']);
         return `Saved song/playlist as ${argv['name']}`
     }, ['url', 'name'], "Saves the YouTube song/playlist with a specific name");
@@ -153,10 +61,11 @@ client.on('message', msg => {
             return;
         }
         logger.verbose(`<${msg.author.username}>: ${msg.content}`);
-        let reply = cmd.parseMessage(msg);
-        if (reply) {
-            msg.reply(reply);
-            return;
+        if (!msg.guild) {
+            let reply = cmd.parseMessage(msg);
+            if (reply) msg.channel.send(reply);
+        } else {
+            guilding.getHandler(msg.guild, prefix).handleMessage(msg);
         }
     } catch (err) {
         logger.error(err.stack);
