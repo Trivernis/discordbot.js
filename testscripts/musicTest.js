abd3ccb4c6ddce1fc0bb9a9af3117636011cb42d
Julius Riegel <jriegel@t-h.de>
2019-01-23 08:49:00 +0100

    Changed musicTest configuration

--- testscripts/musicTest.js
+++ testscripts/musicTest.js
@@ -28,5 +28,11 @@ function main() {
 
 // Executing the main function
 if (typeof require !== 'undefined' && require.main === module) {
+    process.on('unhandledRejection', (reason, p) => {
+        console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
+        throw Error('Promise rejection');
+    });
+
+    setTimeout(() => process.exit(1), 60000);
     main();
 }
